0bd37ba6bb7d8d2405755d151bde149ab95c8cec
CodeDr <34587303+CodeDrTech@users.noreply.github.com>
2023-10-31 21:05:35 -0400

    jp

--- db/Tables.sql
+++ db/Tables.sql
@@ -1,85 +1,17 @@
-CREATE DATABASE [sistemaDePrestamos]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'sistemaDePrestamos', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\sistemaDePrestamos.mdf' , SIZE = 5120KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
- LOG ON 
-( NAME = N'sistemaDePrestamos_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL12.SQLEXPRESS\MSSQL\DATA\sistemaDePrestamos_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+/****** Primero crea la base de datos ******/
+USE [master]
 GO
-ALTER DATABASE [sistemaDePrestamos] SET COMPATIBILITY_LEVEL = 120
+
+CREATE DATABASE [Ventas]
+
+
+
+
+/****** Segundo crea las tablas ******/
+USE [Loansystem]
 GO
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [sistemaDePrestamos].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
-ALTER DATABASE [sistemaDePrestamos] SET ANSI_NULL_DEFAULT OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET ANSI_NULLS OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET ANSI_PADDING OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET ANSI_WARNINGS OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET ARITHABORT OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET AUTO_CLOSE OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET AUTO_SHRINK OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET AUTO_UPDATE_STATISTICS ON 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET CURSOR_DEFAULT  GLOBAL 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET NUMERIC_ROUNDABORT OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET QUOTED_IDENTIFIER OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET RECURSIVE_TRIGGERS OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET  DISABLE_BROKER 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET TRUSTWORTHY OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET PARAMETERIZATION SIMPLE 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET HONOR_BROKER_PRIORITY OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET RECOVERY SIMPLE 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET  MULTI_USER 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET PAGE_VERIFY CHECKSUM  
-GO
-ALTER DATABASE [sistemaDePrestamos] SET DB_CHAINING OFF 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET TARGET_RECOVERY_TIME = 0 SECONDS 
-GO
-ALTER DATABASE [sistemaDePrestamos] SET DELAYED_DURABILITY = DISABLED 
-GO
-USE [sistemaDePrestamos]
-GO
-/****** Object:  Table [dbo].[Clietes]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_PADDING ON
-GO
-CREATE TABLE [dbo].[Clietes](
+
+CREATE TABLE [dbo].[Clientes](
 	[idCliente] [int] IDENTITY(1,1) NOT NULL,
 	[Nombre] [varchar](50) NOT NULL,
 	[Telefono] [varchar](50) NULL,
@@ -99,15 +31,8 @@ CREATE TABLE [dbo].[Clietes](
 	[idCliente] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
 
-GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[cuotas]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[cuotas](
 	[id_cuota] [int] IDENTITY(1,1) NOT NULL,
 	[no_cuota] [int] NULL,
@@ -124,13 +49,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 
 GO
-/****** Object:  Table [dbo].[Empleado]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_PADDING ON
-GO
+
 CREATE TABLE [dbo].[Empleado](
 	[idEmpleado] [int] IDENTITY(1,1) NOT NULL,
 	[Nombre] [varchar](50) NULL,
@@ -150,13 +69,7 @@ UNIQUE NONCLUSTERED
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[EmpleadoImagenes]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+
 CREATE TABLE [dbo].[EmpleadoImagenes](
 	[IdImagenes] [int] NOT NULL,
 	[Imagen] [image] NOT NULL,
@@ -168,13 +81,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
-/****** Object:  Table [dbo].[Empresas]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_PADDING ON
-GO
+
 CREATE TABLE [dbo].[Empresas](
 	[idEmpresa] [int] NOT NULL,
 	[Nombre] [varchar](50) NULL,
@@ -189,15 +96,7 @@ CREATE TABLE [dbo].[Empresas](
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[garante]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_PADDING ON
-GO
+
 CREATE TABLE [dbo].[garante](
 	[idGarante] [int] IDENTITY(1,1) NOT NULL,
 	[Nombre] [varchar](50) NULL,
@@ -214,15 +113,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[Garantes]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_PADDING ON
-GO
+
 CREATE TABLE [dbo].[Garantes](
 	[idGarantes] [int] NOT NULL,
 	[Nombre] [varchar](50) NULL,
@@ -239,15 +130,7 @@ CREATE TABLE [dbo].[Garantes](
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[Notificacion]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-SET ANSI_PADDING ON
-GO
+
 CREATE TABLE [dbo].[Notificacion](
 	[idNotificacion] [int] IDENTITY(1,1) NOT NULL,
 	[tipo] [int] NULL,
@@ -263,13 +146,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 
 GO
-SET ANSI_PADDING OFF
-GO
-/****** Object:  Table [dbo].[solicitud_prestamo]    Script Date: 04/07/2018 02:12:01 p.m. ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+
 CREATE TABLE [dbo].[solicitud_prestamo](
 	[id_solicitud] [int] IDENTITY(1,1) NOT NULL,
 	[cantidad_cuota] [int] NOT NULL,
@@ -287,6 +164,7 @@ PRIMARY KEY CLUSTERED
 ) ON [PRIMARY]
 
 GO
+
 ALTER TABLE [dbo].[cuotas]  WITH CHECK ADD FOREIGN KEY([prestamo])
 REFERENCES [dbo].[solicitud_prestamo] ([id_solicitud])
 GO
@@ -300,6 +178,4 @@ ALTER TABLE [dbo].[solicitud_prestamo]  WITH CHECK ADD FOREIGN KEY([garantes])
 REFERENCES [dbo].[garante] ([idGarante])
 GO
 USE [master]
-GO
-ALTER DATABASE [sistemaDePrestamos] SET  READ_WRITE 
 GO
